6acde09e2fc38b91c54f5198a7ab33c2c5372c13
Alexis Hernandez <alexis22229@gmail.com>
2023-02-01 06:46:45 -0700

    Initial cli PoC
    Just a hello world through a CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct CliArgs {
+    #[arg(short, long)]
+    name: String,
+
+    #[arg(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = CliArgs::parse();
+
+    for _ in 0..args.count {
+        println!("Hello {}!", args.name)
+    }
 }
