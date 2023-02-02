6a669183afd48cdb8230a90c4167d45556afa05f
Alexis Hernandez <alexis22229@gmail.com>
2023-02-02 06:45:12 -0700

    Add simple test

--- src/main.rs
+++ src/main.rs
@@ -10,10 +10,27 @@ struct CliArgs {
     count: u8,
 }
 
+fn hello(name: &String) -> String {
+    format!("Hello {}!", name)
+}
+
 fn main() {
     let args = CliArgs::parse();
 
     for _ in 0..args.count {
-        println!("Hello {}!", args.name)
+        let msg = hello(&args.name);
+        println!("{msg}");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::hello;
+
+    #[test]
+    fn it_works() {
+        let input = String::from("cli");
+        let result = hello(&input);
+        assert_eq!(result, "Hello cli!");
     }
 }
